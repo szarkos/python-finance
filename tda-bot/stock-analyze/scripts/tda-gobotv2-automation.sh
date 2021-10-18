e85ce18ace6fc12042c6e3a5627a23425bfa8d87
Stephen A. Zarkos <steve@sentry.net>
2021-10-18 21:48:12 +0000

    Updates to test scripts and automation

---
+++ tda-bot/stock-analyze/scripts/tda-gobotv2-automation.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# This is just a small script to kick off the gobot
+
+time_wait=${1-"1"}
+sleep $time_wait
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "${parent_path}/../../"
+
+source ./stock-analyze/tickers.conf
+tickers=$CUR_SET
+
+nohup ./tda-stochrsi-gobot-v2.py --stoploss --stock_usd=5000 --stocks=${tickers} --short --singleday \
+	--decr_threshold=0.4 --incr_threshold=0.5 --max_failed_txs=2 --exit_percent=1 \
+	--algos=algo_id:main,primary_stochrsi,dmi_simple,aroonosc,adx,support_resistance,adx_threshold:6 \
+	\
+	--algos=algo_id:stochrsi_stochmfi_chop_simple,\
+primary_stochrsi,stochmfi,chop_simple,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algos=algo_id:stochrsi_stochmfi_chop_index,\
+primary_stochrsi,stochmfi,chop_index,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algos=algo_id:stochrsi_stochmfi_chop_simple_mfi,\
+primary_stochrsi,stochmfi,chop_simple,mfi,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algos=algo_id:stochrsi_stochmfi_chop_index_mfi,\
+primary_stochrsi,stochmfi,chop_index,mfi,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algos=algo_id:stochrsi_stochmfi_macd,\
+primary_stochrsi,stochmfi,macd,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+macd_short_period:42,macd_long_period:52,macd_signal_period:5,macd_offset:0.00475,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algos=algo_id:stochrsi_stochmfi_macd_chop_simple,\
+primary_stochrsi,stochmfi,macd,chop_simple,adx,support_resistance,\
+rsi_high_limit:80,rsi_low_limit:20,stochmfi_period:128,stochrsi_offset:1.5,\
+macd_short_period:42,macd_long_period:52,macd_signal_period:5,macd_offset:0.00475,\
+adx_threshold:7,max_daily_natr:3.5,min_intra_natr:0.15,max_intra_natr:0.4\
+	\
+	--algo_valid_tickers=stochrsi_stochmfi_chop_simple:LOW,C,WDC,RTX,TPR,ZM,NLOK,IR,MSFT,GIS,CRWD,WELL,VTR,TXN,GILD,CF,CAG \
+	--algo_valid_tickers=stochrsi_stochmfi_chop_index:MT,MS,PYPL,MRVL,ABNB,BX,BP,GLW,FITB,BIDU,BEN,ZM,RCL,HUN,GM,DISCA,AVTR,INTC,DDOG \
+	--algo_valid_tickers=stochrsi_stochmfi_chop_simple_mfi:MT,OKE,FITB,AIG,ADI,RCL,LOW,ZM,TPR,GIS,DIS,DBX,CAT \
+	--algo_valid_tickers=stochrsi_stochmfi_chop_index_mfi:UBER,XOM,MOS,BEN,ATVI,WFC,PYPL,MS,CFG,AAPL \
+	--algo_valid_tickers=stochrsi_stochmfi_macd:BA,FB,UNH,EXPE \
+	--algo_valid_tickers=stochrsi_stochmfi_macd_chop_simple:BA,FB,UNH,EXPE \
+	\
+	--rsi_high_limit=85 --rsi_low_limit=15 --stochrsi_offset=3 \
+	--daily_atr_period=7 --min_intra_natr=0.15 --min_daily_natr=1.5 \
+	--aroonosc_with_macd_simple --variable_exit --lod_hod_check --use_natr_resistance \
+	--weekly_ifile=stock-analyze/weekly-csv/TICKER-weekly-2019-2021.pickle \
+	--daily_ifile=stock-analyze/daily-csv/TICKER-daily-2019-2021.pickle \
+	--tx_log_dir=TX_LOGS_v2 1> logs/gobot-v2.log 2>&1 &
+
+disown
+
+#	--algos=primary_stochrsi,mfi,dmi_simple,aroonosc,adx,support_resistance,adx_threshold:6 \
+#	--algos=primary_stochrsi,dmi_simple,aroonosc,adx,support_resistance,adx_threshold:6,stochrsi_offset:12 \
+#	--algos=primary_stochrsi,mfi,aroonosc,adx,support_resistance,mfi_high_limit:95,mfi_low_limit:5,adx_threshold:20,adx_period:48 \
+#	--algos=primary_stochrsi,rsi,mfi,adx,support_resistance,adx_threshold:20,mfi_high_limit:95,mfi_low_limit:5 \
+
--- tda-bot/stock-analyze/scripts/testdaily-best_stocks.sh
+++ tda-bot/stock-analyze/scripts/testdaily-best_stocks.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# By default, $start_date is two weeks ago from today
-start_date=${1-$(date +'%Y-%m-%d' --date='-1 weeks')}
+# By default, $start_date is three weeks ago from today
+start_date=${1-$(date +'%Y-%m-%d' --date='-3 weeks')}
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${parent_path}/.."
 
 # Gain threshold - net gain (per share) required to add stock to the final list
-# Typically algo1 is less restrictive and more risky, so only include tickers
-#  that have a higher gain threshold.
 algo1_gain_threshold="0.2"
-algo2_gain_threshold="0.08"
 
 # First, refresh all the 3-month data
 ./cron/refresh-data.sh
@@ -18,7 +15,7 @@ algo2_gain_threshold="0.08"
 
 # Run the test script for each ticker
 source ./tickers.conf
-tickers=$BIGLIST
+tickers=$( ./filter-stocks.py --blacklist --stocks=$BIGLIST | tr '\n' ' ' | sed 's/\s//g' )
 tickers=$( echo -n $tickers | sed 's/,/ /g' )
 
 if [ "$tickers" == "" ]; then
@@ -40,31 +37,24 @@ for t in $tickers; do
 
 	echo $t;
 	./gobot-test.py --all --ifile=stock-analyze/monthly-1min-csv/${t}-3months-${end_date}.pickle --ofile=stock-analyze/results/${t} \
-		--opts=" --weekly_ifile=stock-analyze/weekly-csv/${t}-weekly-2019-2021.pickle --exit_percent=1 $startdate --skip_blacklist "
+		--opts=" --weekly_ifile=stock-analyze/weekly-csv/${t}-weekly-2019-2021.pickle --daily_ifile=./daily-csv/${t}-daily-2019-2021.pickle \
+		--skip_blacklist $startdate " --scenarios=standard_daily_test
 
 done
 cd "${parent_path}/.."
 
-
 # Generate results summary
 data=$( ./summarize-results.sh ticker-net-gain )
 
-
 # Parse and sort results
-# This part is very dependent on the notion that we are only testing for two good
-#  indicator groups (i.e. stochrsi_dmi_simple and stochrsi_aroonosc_dmi_simple)
 algo1=$( for i in "$data"; do echo "$i" | awk -F ',' '{print $1","$4}'; done )
-algo2=$( for i in "$data"; do echo "$i" | awk -F ',' '{print $1","$7}'; done )
-
 algo1=$( echo "$algo1" | sort -bt ',' -k1,2 )
-algo2=$( echo "$algo2" | sort -bt ',' -k1,2 )
 
 
 # Create list of tickers that lost money
 loss_list=""
 algo1_loss=$( for i in "$data"; do echo "$i" | awk -F ',' '{print $1","$3}'; done )
-algo2_loss=$( for i in "$data"; do echo "$i" | awk -F ',' '{print $1","$6}'; done )
-for i in $( echo -e "${algo1_loss}\n${algo2_loss}" ) ; do
+for i in $algo1_loss ; do
 	net_loss=$(echo -n $i | awk -F, '{print $2}' )
 
 	if `echo -n $net_loss | grep --silent 'stochrsi'` ; then
@@ -101,51 +91,6 @@ for i in $( echo -e "${algo1}" ) ; do
 		fi
 	done
 	if [ "$i" == "" ]; then
-		continue
-	fi
-
-	# Check if ticker is currently blacklisted
-	cd ${parent_path}/../../
-	blacklist=$( ./tda-quote-stock.py --check_blacklist $ticker )
-	cd "${parent_path}/.."
-	if [ "$blacklist" == "True" ]; then
-		continue
-	fi
-
-	# Finally, add stock to the list if it has made it this far
-	list="$list "$(echo $i | awk -F, '{print $1}')
-
-done
-
-## ALGO2
-for i in $( echo -e "${algo2}" ) ; do
-	if `echo -n $i | grep --silent 'stochrsi'` ; then
-		continue
-	fi
-
-	# Check if it's net gain is below the $gain_threshold
-	gain=$(echo -n $i | awk -F, '{print $2}' )
-	if (( $( echo "$gain" ' < ' "$algo2_gain_threshold" | bc -l ) )); then
-		continue
-	fi
-
-	# Check if ticker has been listed in $loss_list
-	ticker=$(echo -n $i | awk -F, '{print $1}' )
-	for t in "$loss_list"; do
-		if [ "$ticker" == "$t" ]; then
-			i=""
-			break
-		fi
-	done
-	if [ "$i" == "" ]; then
-		continue
-	fi
-
-	# Check if ticker is currently blacklisted
-	cd ${parent_path}/../../
-	blacklist=$( ./tda-quote-stock.py --check_blacklist $ticker )
-	cd "${parent_path}/.."
-	if [ "$blacklist" == "True" ]; then
 		continue
 	fi
 

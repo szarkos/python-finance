4cc3931e8cc0a7f62fa6f838042d89dc8f94bc4c
Stephen A. Zarkos <steve@sentry.net>
2021-12-03 01:09:19 +0000

    Add --max_intra_natr=1 to automation script

--- tda-bot/stock-analyze/scripts/tda-gobotv2-automation.sh
+++ tda-bot/stock-analyze/scripts/tda-gobotv2-automation.sh
@@ -29,14 +29,14 @@ mkdir ./logs 2>/dev/null
 source ./stock-analyze/tickers.conf
 tickers=$CUR_SET
 
-nohup ./tda-stochrsi-gobot-v2.py --stoploss --stock_usd=30000 --stocks=${tickers} --short --singleday \
+nohup ./tda-stochrsi-gobot-v2.py --stoploss --stock_usd=20000 --stocks=${tickers} --short --singleday \
 	--decr_threshold=1.5 --incr_threshold=0.5 --max_failed_txs=2 --exit_percent=0.5 \
 	\
 	--algos=algo_id:stackedma_kama_wma,primary_stacked_ma,stacked_ma_type_primary:kama,stacked_ma,stacked_ma_type:wma,bbands_kchannel,support_resistance,use_trend,use_combined_exit,use_bbands_kchannel_xover_exit,min_intra_natr:0.65,min_daily_natr:6 \
 	\
 	--algo_exclude_tickers=stackedma_kama_wma:${STACKEDMA_KAMA_WMA_EXCLUDE} \
 	\
-	--stacked_ma_periods_primary=8,13,21 --stacked_ma_periods=34,55,89 \
+	--stacked_ma_periods_primary=8,13,21 --stacked_ma_periods=34,55,89 --max_intra_natr=1 \
 	--bbands_kchannel_offset=0.15 --bbands_kchan_squeeze_count=8 \
 	--rsi_high_limit=75 --rsi_low_limit=25 --stochrsi_offset=3 --daily_atr_period=3  \
 	--aroonosc_with_macd_simple --variable_exit --lod_hod_check --use_combined_exit \

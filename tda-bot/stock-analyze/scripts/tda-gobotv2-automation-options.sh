62e79935d8b4868724730a835481c82eb20564aa
Stephen A. Zarkos <steve@sentry.net>
2022-04-22 03:42:33 +0000

    Automation update

--- tda-bot/stock-analyze/scripts/tda-gobotv2-automation-options.sh
+++ tda-bot/stock-analyze/scripts/tda-gobotv2-automation-options.sh
@@ -28,32 +28,34 @@ fi
 mkdir ./logs 2>/dev/null
 source ./stock-analyze/tickers.conf
 tickers=$CUR_SET
-tickers="SPY,QQQ,TQQQ"
+tickers="SPY,QQQ"
 
 nohup ./tda-stochrsi-gobot-v2.py --stoploss --stocks=${tickers} --short --unsafe --fake --singleday \
 	--decr_threshold=0.2 --incr_threshold=0.25 --exit_percent=0.4 --max_failed_txs=5 --stock_usd=20000 \
-	--options_decr_threshold=5 --options_incr_threshold=2 --options_exit_percent=5 --options_usd=1000 \
+	--options_decr_threshold=10 --options_incr_threshold=10 --options_exit_percent=20 --options_usd=1000 \
 	--resist_pct_dynamic --price_resistance_pct=0.25 --price_support_pct=0.25 \
 	\
-	--algos=algo_id:trintick_scalp_options,primary_trin,tick,use_keylevel,va_check,trin_ma_type:trima,trin_ma_period:8,options,quick_exit,quick_exit_percent:2 \
-	--algos=algo_id:stochrsi_scalp_options,primary_stochrsi,tick,trin,use_keylevel,va_check,rsi_period:14,stochrsi_period:14,stochrsi_offset:0,trin_ma_type:trima,trin_ma_period:8,options,quick_exit,quick_exit_percent:2 \
+	--algos=algo_id:sp_monitor_options,primary_sp_monitor,sp_monitor_tickers:AAPL:6.88+MSFT:5.63+AMZN:3.56+TSLA:2.21+GOOGL:2.05+GOOG:1.9+BRK.B:1.69+NVDA:1.43+UNH:1.35+FB:1.3+JNJ:1.27+PG:1.02+JPM:1+XOM:1+V:0.95+CVX:0.89+HD:0.85+MA:0.83+PFE:0.8+ABBV:0.77,sp_monitor_use_trix,sp_monitor_trix_ma_type:hma,sp_monitor_trix_ma_period=14,use_keylevel,va_check,use_pdc,use_vwap,use_natr_resistance,options,quick_exit,quick_exit_percent:20 \
 	\
-	--algo_valid_tickers=trintick_scalp_options:SPY,QQQ \
-	--algo_valid_tickers=stochrsi_scalp_options:SPY,QQQ \
-	\
-	--algos=algo_id:TQQQ_trintick_scalp_options,primary_trin,tick,use_keylevel,keylevel_use_daily,va_check,support_resistance,trin_ma_type:hma,trin_ma_period:5,options,quick_exit,quick_exit_percent:2 \
-	--algos=algo_id:TQQQ_stochrsi_scalp_options,primary_stochrsi,tick,trin,use_keylevel,keylevel_use_daily,va_check,support_resistance,rsi_period:14,stochrsi_period:14,stochrsi_offset:0,trin_ma_type:hma,trin_ma_period:5,options,quick_exit,quick_exit_percent:2 \
-	\
-	--algo_valid_tickers=TQQQ_trintick_scalp_options:TQQQ \
-	--algo_valid_tickers=TQQQ_stochrsi_scalp_options:TQQQ \
+	--algo_valid_tickers=sp_monitor_options:SPY,QQQ \
 	\
 	--tx_log_dir=TX_LOGS_v2 1> logs/gobot-v2.log 2>&1 &
 
 disown
 
-#--trend_quick_exit --qe_stacked_ma_type=vidya --use_combined_exit \
-#--algos=algo_id:trin_tick_options,primary_trin,tick,use_keylevel,keylevel_use_daily,keylevel_strict,va_check,options,roc_exit,roc_period:38,quick_exit,quick_exit_percent:2 \
-#--algos=algo_id:stochrsi_options,primary_stochrsi,tick,trin,use_keylevel,keylevel_use_daily,keylevel_strict,va_check,rsi_period:14,stochrsi_period:14,stochrsi_offset:4,options,quick_exit_percent:10 \
 
-#--algos=algo_id:trin_tick_options,primary_trin,tick,use_keylevel,keylevel_use_daily,keylevel_strict,va_check,options,roc_exit,roc_period:38,quick_exit,quick_exit_percent:2 \
-#--algos=algo_id:trin_tick_options,primary_trin,tick,use_keylevel,keylevel_use_daily,keylevel_strict,va_check,options,near_expiration,roc_exit,roc_period:38,quick_exit_percent:10 \
+#--algos=algo_id:sp_monitor_tick_options,primary_sp_monitor,sp_monitor_tickers:AAPL:6.88+MSFT:5.63+AMZN:3.56+TSLA:2.21+GOOGL:2.05+GOOG:1.9+BRK.B:1.69+NVDA:1.43+UNH:1.35+FB:1.3+JNJ:1.27+PG:1.02+JPM:1+XOM:1+V:0.95+CVX:0.89+HD:0.85+MA:0.83+PFE:0.8+ABBV:0.77,sp_monitor_use_trix,sp_monitor_trix_ma_type:hma,sp_monitor_trix_ma_period=14,tick:tick_ma_type:ema,tick_ma_period:5,use_keylevel,va_check,use_pdc,use_vwap,use_natr_resistance,options,quick_exit,quick_exit_percent:20 \
+
+
+
+#	--algos=algo_id:trintick_scalp_options,primary_trin,tick,use_keylevel,va_check,trin_ma_type:trima,trin_ma_period:8,options,quick_exit,quick_exit_percent:2 \
+#	--algos=algo_id:stochrsi_scalp_options,primary_stochrsi,tick,trin,use_keylevel,va_check,rsi_period:14,stochrsi_period:14,stochrsi_offset:0,trin_ma_type:trima,trin_ma_period:8,options,quick_exit,quick_exit_percent:2 \
+#	\
+#	--algo_valid_tickers=trintick_scalp_options:SPY,QQQ \
+#	--algo_valid_tickers=stochrsi_scalp_options:SPY,QQQ \
+#	\
+#	--algos=algo_id:TQQQ_trintick_scalp_options,primary_trin,tick,use_keylevel,keylevel_use_daily,va_check,support_resistance,trin_ma_type:hma,trin_ma_period:5,options,quick_exit,quick_exit_percent:2 \
+#	--algos=algo_id:TQQQ_stochrsi_scalp_options,primary_stochrsi,tick,trin,use_keylevel,keylevel_use_daily,va_check,support_resistance,rsi_period:14,stochrsi_period:14,stochrsi_offset:0,trin_ma_type:hma,trin_ma_period:5,options,quick_exit,quick_exit_percent:2 \
+#	\
+#	--algo_valid_tickers=TQQQ_trintick_scalp_options:TQQQ \
+#	--algo_valid_tickers=TQQQ_stochrsi_scalp_options:TQQQ \
